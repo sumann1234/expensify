90836241c3dca1f21a3d0c6b67647bfd8b64c918
Suman <suman15432@gmail.com>
2020-04-09 17:20:07 +0545

    remove dummy exchange data

--- Src/app.js
+++ Src/app.js
@@ -13,23 +13,7 @@ import AppRouter from './routers/AppRouters';
 
 const store =configureStore();
 
-store.subscribe(() =>
-{
-    const state = store.getState();
-    const visibile= visibleExpense(state.expense,state.filter);
-    console.log(visibile);
 
-})
-
-store.dispatch(addExpense({description:'Gas Bill',amount:900, createdAt:2}));
-store.dispatch(addExpense({description:'Jas Bill',amount:100, createdAt:50}));
-store.dispatch(addExpense({description:'Water Bill',amount:300, createdAt:9}));
-// store.dispatch(setTextFilter('gas'));
-
-
-// setTimeout(() =>{
-//     return store.dispatch(setTextFilter('Bill'));
-// },3000)
 
 const jsx =(
     <Provider store={store}>
