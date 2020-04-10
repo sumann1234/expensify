32bcead19f486a0d8d37ad07781125b8dd3efe87
Suman <suman15432@gmail.com>
2020-04-10 20:38:47 +0545

    summary complete

--- Src/components/ExpenseDashBoardPage.js
+++ Src/components/ExpenseDashBoardPage.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
+import ExpensesSummary from './ExpenseSummary';
 
 const ExpenseDashBoardPage =() =>(
+    <div>
+    <ExpensesSummary />
     <ExpenseList />
+    </div>
+    
 );
 
 export default ExpenseDashBoardPage;
